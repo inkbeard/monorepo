6a11eb244655473db773e85c7607667b9d6cb3b7
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-01-17 16:17:42 -0600

    [ink-41] added AppIcon
    Change-Id: Ibe995d7578099105c8b5b45791b372e9fe78046f

---
+++ .changeset/lemon-carpets-confess.md
@@ -0,0 +1,6 @@
+---
+"inkbeard": minor
+---
+
+- Added AppIcon component and test
+- Added unit test run back into ci
---
+++ apps/inkbeard/src/components/AppIcon.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+  defineProps({
+    /**
+     * The icon type (fa-solid, fa-thin, etc) and icon name (fa-t-rex, fa-bulldozer, etc) to render.
+     */
+    icon: {
+      type: String,
+      required: true,
+    },
+    /**
+     * The appended icon from the the fontawesome [icon size property](https://fontawesome.com/v6/docs/web/style/size) without the `fa-` prefix.
+     */
+    size: {
+      type: String,
+      default: 'lg',
+      validator: (value: string) => (
+        [
+          // relative sizes
+          '2xs',
+          'xs',
+          'sm',
+          'lg',
+          'xl',
+          '2xl',
+          // literal sizes
+          '1x',
+          '2x',
+          '3x',
+          '4x',
+          '5x',
+          '6x',
+          '7x',
+          '8x',
+          '9x',
+          '10x',
+        ].includes(value)
+      ),
+    },
+    /**
+     * The color theme used to render the icon color.
+     */
+    type: {
+      type: String,
+      default: '',
+      validator: (value: string) => (
+        [
+          '',
+          'danger',
+          'info',
+          'success',
+          'warning',
+        ].includes(value)
+      ),
+    },
+  });
+</script>
+
+<template>
+  <i
+    :class="[
+      icon,
+      'app-icon',
+      `fa-${size}`,
+      {
+        [`is-${type}`]: type,
+      },
+    ]"
+  />
+</template>
+
+<style scoped>
+  .is-danger {
+    color: var(--ink-danger);
+  }
+
+  .is-info {
+    color: var(--ink-info);
+  }
+
+  .is-success {
+    color: var(--ink-success);
+  }
+
+  .is-warning {
+    color: var(--ink-warning);
+  }
+</style>
