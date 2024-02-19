558f44128a851e1d273145c2fda9daa16319cd0a
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-02-19 16:39:05 -0600

    Replace buttons on SourceListing and SourceEditor.
    Change-Id: I2360fc22f2ed32d3a161ef908315f31854372db4

---
+++ .changeset/lazy-ways-do.md
@@ -0,0 +1,6 @@
+---
+"@inkbeard/ui-vue": minor
+---
+
+- Added size prop to AddButton
+- Updated the theme to be blue instead of green. This distinguishes the success severity from from the primary button color.
--- packages/ui-vue/src/components/AppButton.vue
+++ packages/ui-vue/src/components/AppButton.vue
@@ -4,9 +4,13 @@
   defineOptions({
     inheritAttrs: false,
   });
-  defineProps<{
-    isSmall?: boolean;
-  }>();
+  defineProps({
+    size: {
+      type: String,
+      default: 'md',
+      validator: (value: string) => ['sm', 'md', 'lg'].includes(value),
+    },
+  });
 </script>
 
 <template>
@@ -14,8 +18,23 @@
     v-bind="$attrs"
     :pt="{
       root: {
-        class: `app-button ${isSmall ? 'p-button-sm' : ''}`,
+        class: `app-button p-button-${size}`,
       },
     }"
   />
 </template>
+
+<style>
+.btn-group {
+  display: flex;
+  justify-content: center;
+
+  &.align-end {
+    justify-content: flex-end;
+  }
+
+  &.align-start {
+    justify-content: flex-start;
+  }
+}
+</style>
