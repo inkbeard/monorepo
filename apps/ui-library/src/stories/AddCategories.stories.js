d13c9a1e389ada81d7c6b0a67144ac7d8440374e
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-05 15:09:46 -0600

    Added AddExpense component
    Change-Id: I11b94a13c36ec5a9338d982ddd1547102ae877ca

---
+++ apps/ui-library/src/stories/AddCategories.stories.js
@@ -0,0 +1,45 @@
+import { AddCategory } from '@inkbeard/budget-it';
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+export default {
+  title: 'Budget It/AddCategory',
+  component: AddCategory,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'padded',
+  },
+};
+export const AddCategoryComponent = {
+  args: {
+    categoryList: [
+      {
+        name: 'Entertainment',
+        id: 1,
+      },
+      {
+        name: 'Food',
+        id: 2,
+      },
+      {
+        name: 'Housing',
+        id: 3,
+      },
+      {
+        name: 'Transportation',
+        id: 4,
+      },
+      {
+        name: 'Utilities',
+        id: 5,
+      },
+      {
+        name: 'Clothing',
+        id: 6,
+      },
+      {
+        name: 'Medical',
+        id: 7,
+      },
+    ],
+  },
+};
