af0d64b9f5ed9b5d2ab8c89e562a583e33c5d112
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-06 14:43:08 -0600

    added pencil icon to edit expense (#239)

--- packages/budget-it/src/components/ExpenseItem.vue
+++ packages/budget-it/src/components/ExpenseItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { inject, ref } from 'vue';
-  import { AppInputNumber, AppDropdown } from '@inkbeard/ui-vue';
+  import { AppButton, AppInputNumber, AppDropdown } from '@inkbeard/ui-vue';
   import type { ExpenseInfo, LabelsAndIds } from '../types';
 
   const expense = defineModel<ExpenseInfo>('expense', { required: true });
@@ -10,34 +10,44 @@
 </script>
 
 <template>
-  <form @submit.prevent>
-    <AppInputNumber
-      :id="`${expense.categoryId}-${expense.name}`"
-      v-model="expenseAmount"
-      :input-id="`${expense.categoryId}-${expense.name}`"
-      :label="expense.name"
-      :label-description="expense.description"
-      @blur="expense.amount = expenseAmount"
-    />
-    <AppDropdown
-      :key="expense.sourceId"
-      v-model="expense.sourceId"
-      input-id="expense-source"
-      label="Source"
-      option-label="label"
-      option-value="id"
-      :options="alphabaticSourceList"
-    />
-  </form>
+  <div class="expense-item">
+    <form class="expense-item-" @submit.prevent>
+      <AppInputNumber
+        :id="`${expense.categoryId}-${expense.name}`"
+        v-model="expenseAmount"
+        :input-id="`${expense.categoryId}-${expense.name}`"
+        :label="expense.name"
+        :label-description="expense.description"
+        @blur="expense.amount = expenseAmount"
+      />
+      <AppDropdown
+        :key="expense.sourceId"
+        v-model="expense.sourceId"
+        input-id="expense-source"
+        label="Source"
+        option-label="label"
+        option-value="id"
+        :options="alphabaticSourceList"
+      />
+    </form>
+    <div class="btn-group">
+      <AppButton icon="fa-solid fa-pencil" text />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.expense-item {
+  display: flex;
+  border-bottom: 1px solid var(--ink-border-color);
+}
+
 form {
   display: flex;
+  flex: 1;
   gap: 1rem;
   padding: 1rem;
-  border-bottom: 1px solid var(--ink-border-color);
-  justify-content: space-evenly;
+  border-right: 1px solid var(--ink-border-color);
 
   > * {
     flex: 1;
