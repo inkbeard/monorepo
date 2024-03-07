2851cd42ecbb0f1e371d93c72e48dac13ce6dd23
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-07 15:31:47 -0600

    Added Dialog to ui-vue export
    Change-Id: Ibce0c276b9460a609aebfc8613d1ac612bc983ea

---
+++ packages/ui-vue/src/components/AppDialog.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+  import Dialog from 'primevue/dialog';
+
+  defineOptions({
+    inheritAttrs: false,
+  });
+</script>
+
+<template>
+  <Dialog
+    v-bind="$attrs"
+    :pt="{
+      root: {
+        class: 'app-dialog',
+      },
+    }"
+  >
+    <template v-for="(value, name) in $slots" #[name]="slotData">
+      <slot :name="name" v-bind="slotData" />
+    </template>
+  </Dialog>
+</template>
