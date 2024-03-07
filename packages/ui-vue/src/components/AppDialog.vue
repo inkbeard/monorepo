87a838255db8dbbe8a4126ded62e5eab9c0e572f
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-07 17:32:16 -0600

    m
    Change-Id: Ia4e993edb811f207be57aeef2b233f7b84488126

--- packages/ui-vue/src/components/AppDialog.vue
+++ packages/ui-vue/src/components/AppDialog.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
   import Dialog from 'primevue/dialog';
+  import AppIcon from './AppIcon.vue';
 
   defineOptions({
     inheritAttrs: false,
@@ -16,7 +17,20 @@
     }"
   >
     <template v-for="(value, name) in $slots" #[name]="slotData">
-      <slot :name="name" v-bind="slotData" />
+      <slot
+        :name="name"
+        v-bind="slotData"
+      />
+    </template>
+    <template #closeicon>
+      <AppIcon icon="fa-solid fa-xmark" />
     </template>
   </Dialog>
 </template>
+
+<style>
+
+.p-dialog-header-close {
+  transform: translate(.5rem, -.5rem);
+}
+</style>
