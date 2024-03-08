e47d42a6f193f3322e7544b593727f5688b8d1f3
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-07 18:02:40 -0600

    [ink-70] Added modal for updating expense name/description (#249)

--- packages/budget-it/src/components/ExpenseItem.vue
+++ packages/budget-it/src/components/ExpenseItem.vue
@@ -2,17 +2,58 @@
   import { inject, ref } from 'vue';
   import {
     AppButton,
+    AppDialog,
     AppInputNumber,
     AppDropdown,
     Tooltip,
+    AppInputText,
   } from '@inkbeard/ui-vue';
   import type { ExpenseInfo, LabelsAndIds } from '../types';
 
+  const emits = defineEmits<{
+    /**
+     * Emit the edited name and description of the expense item.
+     */
+    editExpense: [{
+      id: number,
+      name: string,
+      description: string
+    }]
+  }>();
+
+  const alphabaticSourceList = inject<LabelsAndIds>('alphabaticSourceList', []);
   const expense = defineModel<ExpenseInfo>('expense', { required: true });
   const expenseAmount = ref(expense.value.amount);
-  const alphabaticSourceList = inject<LabelsAndIds>('alphabaticSourceList', []);
+  const expenseName = ref(expense.value.name);
+  const expenseDescription = ref(expense.value.description);
+  const editableExpense = ref({
+    name: expenseName,
+    description: expenseDescription,
+  });
+  const isEditing = ref(false);
   const vTooltip = Tooltip;
 
+  /**
+   * Save the expense with the new information and cancel editing.
+   */
+  function saveExpense() {
+    expense.value.name = expenseName.value;
+    expense.value.description = expenseDescription.value;
+    isEditing.value = false;
+    emits('editExpense', {
+      id: expense.value.id as number,
+      name: editableExpense.value.name,
+      description: editableExpense.value.description || '',
+    });
+  }
+  /**
+   * Cancel the editing of the expense and reset the form.
+   */
+  function cancelEditing() {
+    isEditing.value = false;
+    expenseName.value = expense.value.name;
+    expenseDescription.value = expense.value.description;
+  }
 </script>
 
 <template>
@@ -25,11 +66,14 @@
       >
         {{ expense.name }}
       </h4>
-      <AppButton icon="fa-solid fa-pencil" text />
+      <AppButton
+        icon="fa-solid fa-pencil"
+        text
+        @click="isEditing = true"
+      />
     </div>
     <form class="expense-item-form" @submit.prevent>
       <AppInputNumber
-        :id="`${expense.categoryId}-${expense.name}`"
         v-model="expenseAmount"
         currency="USD"
         :input-id="`${expense.categoryId}-${expense.name}`"
@@ -47,6 +91,46 @@
         :options="alphabaticSourceList"
       />
     </form>
+    <AppDialog
+      v-model:visible="isEditing"
+      :header="`Edit ${expense.name}`"
+      modal
+      :style="{ width: '20rem' }"
+    >
+      <form class="app-form-vertical" @submit.prevent="saveExpense">
+        <AppInputText
+          v-model="editableExpense.name"
+          :input-id="`edit-name-${expense.categoryId}-${expense.name}`"
+          label="Name"
+          label-description="Add a new expense to this category."
+        />
+        <AppInputText
+          v-model="editableExpense.description"
+          :input-id="`edit-description-${expense.categoryId}-${expense.name}`"
+          label="Description"
+          label-description="Add any notes for this expense you would like to remember later."
+        />
+        <div class="btn-group align-end">
+          <AppButton
+            data-test="cancel add expense"
+            icon="fa-solid fa-xmark"
+            label="Cancel"
+            severity="secondary"
+            text
+            @click="cancelEditing"
+          />
+          <AppButton
+            class="submit"
+            data-test="submit add expense"
+            :disabled="!editableExpense.name"
+            icon="fa-solid fa-check"
+            label="Create"
+            severity="primary"
+            @click="saveExpense"
+          />
+        </div>
+      </form>
+    </AppDialog>
   </div>
 </template>
 
@@ -78,7 +162,6 @@
   flex: 1;
   gap: 1rem;
   padding: .0 1rem 1rem;
-  border-right: 1px solid var(--ink-border-color);
 
   > * {
     flex: 1;
