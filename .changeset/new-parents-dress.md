220cdf081776794ce3fb5da3e9572573c028e460
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-11 16:55:12 -0500

    added AppConfirmPopup (#269)

---
+++ .changeset/new-parents-dress.md
@@ -0,0 +1,6 @@
+---
+"@inkbeard/ui-library": minor
+"@inkbeard/ui-vue": minor
+---
+
+- Extended the `ConfirmPopup` as `AppConfirmPopup`
---
+++ apps/ui-library/src/stories/AppConfirmPopup.stories.js
@@ -0,0 +1,46 @@
+import {
+  AppButton,
+  AppConfirmPopup,
+  useConfirm
+} from '@inkbeard/ui-vue';
+
+export const SampleConfirmPopup = {
+  render: (args) => ({
+    components: {
+      AppButton,
+      AppConfirmPopup
+    },
+    setup() {
+      const confirm = useConfirm();
+
+      const showConfirm = ({ currentTarget }) => {
+        confirm.require({
+          target: currentTarget,
+          group: 'testConfirm',
+          message: 'Please confirm to proceed moving forward.',
+          icon: 'fa-solid fa-question-circle',
+          acceptClass: 'p-button-raised p-button-sm',
+          acceptIcon: 'fa-solid fa-check',
+          acceptLabel: 'Confirm',
+          rejectClass: 'p-button-secondary p-button-raised p-button-sm',
+          rejectIcon: 'fa-solid fa-xmark',
+          rejectLabel: 'Cancel',
+          accept: () => {
+            alert('You have accepted');
+          },
+          reject: () => {
+            alert('You have rejected');
+          }
+        });
+      }
+
+      return { args, showConfirm };
+    },
+    template: '<AppConfirmPopup group="testConfirm"/><AppButton label="Click me" @click="showConfirm" />'
+  }),
+};
+
+export default {
+  title: 'ui-vue/AppConfirmPopup',
+  component: AppButton,
+};
