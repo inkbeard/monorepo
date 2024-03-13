ff5fce6fcb3f374788755c7ee62f9bcece8d3b83
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-03-12 21:19:03 -0500

    added story for vue component generator
    Change-Id: I9ec2912411ed64651d46b55ffc724b030f37a2b1

--- packages/ui-vue/index.ts
+++ packages/ui-vue/index.ts
@@ -11,7 +11,6 @@ import AppFormGroup from './src/components/AppFormGroup.vue';
 import AppIcon from './src/components/AppIcon.vue';
 import AppInputNumber from './src/components/AppInputNumber.vue';
 import AppInputText from './src/components/AppInputText.vue';
-import Donkey from './src/components/Donkey.vue';
 
 export {
   AppButton,
