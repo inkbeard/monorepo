d842538999a8f15c42950de92dc47e402ec86205
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-04-01 17:13:16 -0500

    added vitest coverage to budget-it
    Change-Id: I686d9669c4a0f1424e2250b5f483b336afae86ef

--- packages/budget-it/vitest.config.ts
+++ packages/budget-it/vitest.config.ts
@@ -6,8 +6,21 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: 'istanbul',
+        enabled: true,
+        exclude: [
+          ...configDefaults.exclude,
+          'e2e/*',
+          'index.ts',
+          '.eslintrc.cjs',
+        ],
+      },
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
+      exclude: [
+        ...configDefaults.exclude,
+        'e2e/*',
+      ],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
   }),
