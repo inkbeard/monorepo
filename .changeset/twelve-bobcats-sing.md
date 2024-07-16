680c3fb40fa6e39a82418c5af09bd3e070ef17be
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-07-16 18:33:22 -0500

    added AppCard markup, tests, and story (#510)

---
+++ .changeset/twelve-bobcats-sing.md
@@ -0,0 +1,5 @@
+---
+"@inkbeard/ui-vue": minor
+---
+
+Added the AppCard markup, slots, and tests
--- packages/ui-vue/src/components/__tests__/AppCard.spec.ts
+++ packages/ui-vue/src/components/__tests__/AppCard.spec.ts
@@ -5,24 +5,97 @@ import AppCard from '../AppCard.vue';
 describe('AppCard', () => {
   let wrapper: any;
   const createWrapper = ({
-    props,
+    slots,
   }: {
-    props?: {},
+    slots?: {},
   }) => {
     wrapper = shallowMount(
       AppCard,
       {
-        props: { ...props },
+        slots: { ...slots },
       },
     );
   };
 
-  it('renders properly', () => {
-    createWrapper({
-      props: { msg: 'Hello Test!' },
-    });
+  it('should render an empty card with no slots', () => {
+    createWrapper({ });
 
     expect(wrapper.text())
-      .toContain('Hello Test!');
+      .toBe('');
+  });
+
+  it('should render the header slot', () => {
+    const content = 'header slot';
+
+    createWrapper({
+      slots: { header: content },
+    });
+
+    const header = wrapper.find('.app-card-header');
+
+    expect(header.exists())
+      .toBe(true);
+    expect(header.text())
+      .toBe(content);
+  });
+
+  it('should render the title slot', () => {
+    const content = 'title slot';
+
+    createWrapper({
+      slots: { title: content },
+    });
+
+    const title = wrapper.find('.app-card-title');
+
+    expect(title.exists())
+      .toBe(true);
+    expect(title.text())
+      .toBe(content);
+  });
+
+  it('should render the subTitle slot', () => {
+    const content = 'subTitle slot';
+
+    createWrapper({
+      slots: { subTitle: content },
+    });
+
+    const subTitle = wrapper.find('.app-card-sub-title');
+
+    expect(subTitle.exists())
+      .toBe(true);
+    expect(subTitle.text())
+      .toBe(content);
+  });
+
+  it('should render the default slot', () => {
+    const content = 'default slot';
+
+    createWrapper({
+      slots: { default: content },
+    });
+
+    const defaultSlot = wrapper.find('.app-card-description');
+
+    expect(defaultSlot.exists())
+      .toBe(true);
+    expect(defaultSlot.text())
+      .toBe(content);
+  });
+
+  it('should render the footer slot', () => {
+    const content = 'footer slot';
+
+    createWrapper({
+      slots: { footer: content },
+    });
+
+    const footer = wrapper.find('.app-card-footer');
+
+    expect(footer.exists())
+      .toBe(true);
+    expect(footer.text())
+      .toBe(content);
   });
 });
