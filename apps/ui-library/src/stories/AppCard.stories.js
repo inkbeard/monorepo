d1d5446e2c251d00d5cb833ab3bfff154614ac32
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-07-16 17:56:32 -0500

    added AppCard markup, tests, and story
    Change-Id: I8d5be8819ed1b0c92137a4d6d13b2ff41168ff5c

--- apps/ui-library/src/stories/AppCard.stories.js
+++ apps/ui-library/src/stories/AppCard.stories.js
@@ -2,14 +2,59 @@ import { AppCard } from '@inkbeard/ui-vue';
 
 export default {
   title: 'ui vue/AppCard',
-  component: AppCard,
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    header: {
+      control: {
+        type: 'text',
+      },
+    },
+    title: {
+      control: {
+        type: 'text',
+      },
+    },
+    subTitle: {
+      control: {
+        type: 'text',
+      },
+    },
+    default: {
+      control: {
+        type: 'text',
+      },
+    },
+    footer: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  args: {
+    default: 'default slot',
+    footer: 'footer slot',
+    header: 'header slot (most likely an img)',
+    subTitle: 'subTitle slot',
+    title: 'title slot',
+  },
 };
 export const AppCardExample = {
-  args: {
-    msg: 'Hello storybook!',
-  },
+  render: (args) => ({
+    components: { AppCard },
+    setup() {
+      return { args };
+    },
+    template: `
+      <AppCard>
+        <template #header>{{ args.header }}</template>
+        <template #title>{{ args.title }}</template>
+        <template #subTitle>{{ args.subTitle }}</template>
+        <template #default>{{ args.default }}</template>
+        <template #footer>{{ args.footer }}</template>
+      </AppCard>
+    `,
+  }),
 };
--- packages/ui-vue/src/components/AppCard.vue
+++ packages/ui-vue/src/components/AppCard.vue
@@ -2,26 +2,52 @@
   /**
    * A card style container used to display related content.
    */
-  export interface Props {
-    /**
-     * The message displaying in the component.
-     */
-    msg?: string;
-  }
-
-  withDefaults(defineProps<Props>(), {
-    msg: 'Hello AppCard!',
-  });
 </script>
 
 <template>
-  <div class="greetings">
-    <h1>{{ msg }}</h1>
+  <div class="app-card">
+    <div
+      v-if="$slots.header"
+      class="app-card-header"
+    >
+      <slot name="header" />
+    </div>
+    <div class="app-card-body">
+      <div
+        v-if="$slots.title"
+        class="app-card-title"
+      >
+        <slot name="title" />
+      </div>
+      <div
+        v-if="$slots.subTitle"
+        class="app-card-sub-title"
+      >
+        <slot name="subTitle" />
+      </div>
+      <div
+        v-if="$slots.default"
+        class="app-card-description"
+      >
+        <slot />
+      </div>
+    </div>
+    <div
+      v-if="$slots.footer"
+      class="app-card-footer"
+    >
+      <slot name="footer" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-  .greetings {
-    color: var(--ink-success-color);
+  .app-card {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #eaeaea;
+    border-radius: 4px;
+    background-color: #fff;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 10%);
   }
 </style>
