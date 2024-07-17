866681b45dfc0b288922c11503410fb9ca8700b7
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-07-17 17:12:35 -0500

    Added icon memory feature flag and temp components
    Change-Id: I5ed1a0e9b80ba3b5535de3492209bf962f56ff32

--- apps/inkbeard/src/views/IconMemoryView.vue
+++ apps/inkbeard/src/views/IconMemoryView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import ProjectHeader from '@/components/ProjectHeader.vue';
+  import { useFeatureFlagStore } from '@/stores/featureFlags';
+  import TheIconMemoryExample from './TheIconMemoryExample.vue';
 </script>
 
 <template>
@@ -7,6 +9,12 @@
     <template #title>
       Icon Memory Game
     </template>
-    <p>Estimated release: 2024</p>
+    <p>
+      Estimated release: 2024
+    </p>
   </ProjectHeader>
+
+  <template v-if="useFeatureFlagStore().flags.memory_game.enabled">
+    <TheIconMemoryExample />
+  </template>
 </template>
