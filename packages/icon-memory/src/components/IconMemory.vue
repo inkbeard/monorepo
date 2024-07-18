196f0354dd07505bc43c1a24149c00002f8e0daf
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-07-18 14:01:24 -0500

    added IconMemory component and dependency to website
    Change-Id: I2ae6b12b4a2fdb48b340e0e3d7deccc6c6cd4286

---
+++ packages/icon-memory/src/components/IconMemory.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import {
+    AppButton,
+    AppInputNumber,
+  } from '@inkbeard/ui-vue';
+
+  const turnCount = ref(0);
+  const cardCount = ref(4);
+</script>
+
+<template>
+  <AppInputNumber
+    v-model="cardCount"
+    input-id="card-count"
+    label="Pairing count"
+    :min="4"
+    :step="2"
+  />
+  <AppButton
+    label="New turn"
+    raised
+    severity="primary"
+    @click="turnCount++"
+  />
+  <p>Turn count: {{ turnCount }}</p>
+</template>
+
+<style scoped>
+
+</style>
