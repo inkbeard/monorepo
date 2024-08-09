5ebb226e9bbe1d81857258f9babf418f9fc90c7a
Aaron Stoddart <833745+inkbeard@users.noreply.github.com>
2024-08-09 14:38:41 -0500

    Added IconDetails component, tests, and story
    Change-Id: I09ca636af1af97fde5890e6abe662907ac9e7b78

--- packages/icon-memory/index.ts
+++ packages/icon-memory/index.ts
@@ -1,4 +1,5 @@
 import GameSetup from './src/components/GameSetup.vue';
+import IconDetails from './src/components/IconDetails.vue';
 import IconCard from './src/components/IconCard.vue';
 import IconMemory from './src/components/IconMemory.vue';
 import TimeCounter from './src/components/TimeCounter.vue';
@@ -8,6 +9,7 @@ import TurnCounter from './src/components/TurnCounter.vue';
 export {
   GameSetup,
   IconCard,
+  IconDetails,
   IconMemory,
   MatchedCards,
   TimeCounter,
